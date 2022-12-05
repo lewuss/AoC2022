92b6e1631f91b6a7251bc26f316edff59f59c290
lewuss <zenek.zupa@gmail.com>
2022-12-05 13:15:47 +0100

    naprawiony losowy bug z wyniku pep8 xD

--- day4/day4.py
+++ day4/day4.py
@@ -3,12 +3,20 @@ with open('input.txt', 'r') as f:
     for line in f:
         arr.append(line.strip().split(","))
 score = 0
+score_2 = 0
 for elves in arr:
-    start_a = elves[0].split('-')[0]
-    end_a = elves[0].split('-')[1]
-    start_b = elves[1].split('-')[0]
-    end_b = elves[1].split('-')[1]
+    start_a = int(elves[0].split('-')[0])
+    end_a = int(elves[0].split('-')[1])
+    start_b = int(elves[1].split('-')[0])
+    end_b = int(elves[1].split('-')[1])
     if start_a <= start_b and end_a >= end_b:
         score += 1
-    if start_a >= start_b and end_a <= end_b:
+    elif start_a >= start_b and end_a <= end_b:
         score += 1
+
+    if start_a <= start_b <= end_a:
+        score_2 += 1
+    elif end_b >= start_a >= start_b:
+        score_2 += 1
+print(score)
+print(score_2)
